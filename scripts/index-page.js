8bd3767647763ab951ca960fd9fbafff9c225047
Anthony Quispe <71742544+AnthonyQuispe@users.noreply.github.com>
2023-03-21 23:41:53 -0400

    Redid Comment Section in Html

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,66 +1,40 @@
-// Parent Reference
-const parentElement = document.querySelector(".gallery").parentNode;
+let comment = [
+  {
+    name: "Connor Walton",
+    timestamp: "02/17/2021",
+    text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    name: "Emilie Beach",
+    timestamp: "01/09/2021",
+    text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    name: "Miles Acosta",
+    timestamp: "12/20/2020",
+    text: "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough.",
+  },
+];
 
-// Created new Comment Section
-const commentSection = document.createElement("section");
-commentSection.className = "comment";
+function displayComment(comments) {
+  // Created a variable which .comment_bottom
+  const commentBottom = document.querySelector(".comment__bottom");
 
-//Comment Title
-const commentTitle = document.createElement("h2");
-commentTitle.className = "comment__title";
-commentTitle.textContent = "Join the Conversation";
-commentSection.appendChild(commentTitle);
+  // Comment Bottom Div
+  const commentBottomDiv = document.createElement("div");
+  commentBottomDiv.classList.add("comment__bottom--comments");
 
-//Top Div
-const commentTop = document.createElement("div");
-commentTop.className = "comment__top";
-commentSection.appendChild(commentTop);
+  // Comment Bottom Div Left
+  const commentBottomLeft = document.createElement("div");
+  commentBottomLeft.classList.add("comment__bottom--left");
 
-//Top-Left Div
-const commentLeft = document.createElement("div");
-commentLeft.className = "comment__left";
-commentTop.appendChild(commentLeft);
+  const avatar = document.createElement("img");
+  avatars.classList.add("comment__avatar");
+  avatars.setAttribute("src", "/assets/images/Mohan-muruge.jpg");
 
-//Top-Right Div
-const commentRight = document.createElement("div");
-commentRight.className = "comment__right";
-commentTop.appendChild(commentRight);
+  // Comment Bottom Div Right
+  const commentBottomRight = document.createElement("div");
+  commentBottomRight.classList.add("comment__bottom--right");
 
-//Form Field Name
-const formName = document.createElement("label");
-formName.textContent = "NAME";
-const nameField = document.createElement("input");
-nameField.className = "comment__top--name";
-nameField.type = "text";
-nameField.name = "name";
-nameField.placeholder = "Enter your name";
-commentRight.appendChild(formName);
-commentRight.appendChild(nameField);
-
-//Form Field Comment
-const formComment = document.createElement("label");
-formComment.textContent = "COMMENT";
-const commentField = document.createElement("input");
-commentField.className = "comment__top--comment";
-commentField.type = "text";
-commentField.name = "name";
-commentField.placeholder = "Add a new comment";
-commentRight.appendChild(formComment);
-commentRight.appendChild(commentField);
-
-//Button
-const commentButton = document.createElement("input");
-commentButton.className = "comment__top--button";
-commentButton.type = "submit";
-commentButton.value = "COMMENT";
-commentRight.appendChild(commentButton);
-
-//Bottom Div
-const commentBottom = document.createElement("div");
-commentBottom.className = "comment__bottom";
-commentSection.appendChild(commentBottom);
-
-//Insert Comment Section after Gallery Section
-document
-  .querySelector(".gallery")
-  .insertAdjacentElement("afterend", commentSection);
+  commentBottom.appendChild(commenBottomDiv);
+}
